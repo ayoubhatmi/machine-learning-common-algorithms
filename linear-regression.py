6f2ecf9f5c4d548b45e930eb0642982df541d2be
ayoub-hatmi <ayoub.hatmi18@gmail.com>
2023-02-24 12:12:14 +0100

    Add comments

--- linear-regression.py
+++ linear-regression.py
@@ -17,11 +17,13 @@ model = LinearRegression()
 # Split the data into training/testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-
+# Train
 model.fit(X_train, y_train)
+
 predictions = model.predict(X_test)
 print("Predictions : ", predictions)
 
+# Model evaluation
 r_squared = model.score(X_train, y_train)
 print("Coefficient of determination : ", r_squared)
 
